79c473d72e5217932afc4408686dc2e3b73adad3
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-02 15:49:02 -0200

    Links menu mobile adicionados

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@
     // DOM
     let menuBar = document.querySelector('#menuBar');
     let menuMobile = document.querySelector('#menuMobile');
+    let menuLinks = document.querySelector('.mobile-links');
 
     menuBar.addEventListener('click', function () {
         // add class css to button
-        menuMobile.classList.toggle('mobile-show')
+        menuMobile.classList.toggle('mobile-show');
+        menuLinks.classList.toggle('show-links');
     }, false)
 })()
