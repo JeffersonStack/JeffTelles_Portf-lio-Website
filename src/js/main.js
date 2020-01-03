04e5f9bbeff7f40ab3dda8958590f70153a750bc
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-03 12:43:01 -0200

    Icones sociais no menu inseridos

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@
     let menuBar = document.querySelector('#menuBar');
     let menuMobile = document.querySelector('#menuMobile');
     let menuLinks = document.querySelector('.mobile-links');
+    let menuIcons = document.querySelector('.mobile-icons');
 
     menuBar.addEventListener('click', function () {
         // add class css to button
         menuMobile.classList.toggle('mobile-show');
         menuLinks.classList.toggle('show-links');
+        menuIcons.classList.toggle('show-links');
     }, false)
 })()
